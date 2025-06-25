d853279c6d3008d45370398267d56886f1c85319
SaadUrfSadi <urfsaadsadi10@gmail.com>
2025-06-25 18:52:41 +0500

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Navbar.css"
+import macway from "../../Images/macway-logo.png"
+
+function Navbar() {
+  return (
+    <>
+      <section className='container'>
+       <div className='navbar'>
+         <div>
+           <img src={macway} alt="MACWAY LTD" />
+        </div>
+        <div>
+           
+        </div>
+       </div>
+      </section>
+    </>
+  )
+}
+
+export default Navbar
